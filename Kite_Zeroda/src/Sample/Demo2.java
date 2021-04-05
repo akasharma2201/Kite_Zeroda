e9dd9d2f3df9d5563beb22861309b19df3d5243b
Namrata <Namrata@LTPNAMRATA>
2021-04-05 16:10:38 +0530

    [Namrata]- added one class- Demo2

---
+++ Kite_Zeroda/src/Sample/Demo2.java
@@ -0,0 +1,14 @@
+package Sample;
+
+public class Demo2
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("New class added by Namrata at _ 5_April_21");
+		
+		System.out.println("Good AfterNoon");
+	}
+	
+
+}
