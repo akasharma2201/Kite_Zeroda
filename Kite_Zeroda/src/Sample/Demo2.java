ede74755fb8ce082aa8924383ee318c9169d556a
Namrata <Namrata@LTPNAMRATA>
2021-04-05 16:12:08 +0530

    [Namrata]--changes done

--- Kite_Zeroda/src/Sample/Demo2.java
+++ Kite_Zeroda/src/Sample/Demo2.java
@@ -7,6 +7,8 @@ public class Demo2
 		
 		System.out.println("New class added by Namrata at _ 5_April_21");
 		
+		System.out.println("Good Morning");
+		
 		System.out.println("Good AfterNoon");
 	}
 	
